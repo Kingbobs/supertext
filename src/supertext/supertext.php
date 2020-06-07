139d6dd1e28bb45916df32ced2824a165939a8ae
KingBob <52259912+Kingbobs@users.noreply.github.com>
2020-06-06 20:21:18 -0600

    Update supertext.php

--- src/supertext/supertext.php
+++ src/supertext/supertext.php
@@ -5,7 +5,7 @@ use pocketmine\level\particle\FloatingTextParticle;
 use pocketmine\math\Vector3;
 use pocketmine\utils\Config;
 
-class supertext extends pocketmine\plugin\PluginBase::onCommand(pocketmine\command\CommandSender $sender, pocketmine\command\Command $command, string $label, array $args);
+class supertext extends supertext\supertext::onCommand(pocketmine\command\CommandSender $sender, pocketmine\command\Command $command, $label, array $args)
 
 	public function onEnable(){
 		$this->getServer()->getPluginManager()->registerEvents($this, $this);
